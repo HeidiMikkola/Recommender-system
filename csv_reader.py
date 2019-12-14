7448047fc683181eef5fd7b5d98dbc39be91fc96
Ilmari Mäntysaari <mantysaari.ilmari@gmail.com>
2019-12-14 12:30:07 +0200

    Use dict for users instead of array

--- csv_reader.py
+++ csv_reader.py
@@ -3,25 +3,23 @@ import csv
 def load_data(filename):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\t')
-        users = []
-        last_user = ''
+        users = {}
         for row in csv_reader:
-            if row[0] == last_user:
-                users[len(users)-1]['artists'].append({
+            if row[1] in users.keys():
+                users[row[1]]['artists'].append({
                     "id": row[1],
                     "name": row[2],
                     "plays": int(row[3])
                 })
             else:
-                last_user = row[0]
-                users.append({
-                    "id": row[0],
-                    "artists": [{
+                users[row[1]] = {
+                    'artists': [{
                         "id": row[1],
                         "name": row[2],
                         "plays": int(row[3])
                     }]
-                })
+                }
+
         return users
 
 def load_groups():
--- recommender.py
+++ recommender.py
@@ -13,30 +13,41 @@ def farthest_close_neighbor(user):
 
 # naive method for getting n amount of closest neighbors
 def neighbors(data, n):
-    for user in data:
+    for user_key in data:
+        user = data[user_key]
         user["neighbors"] = []
-        
-        for other in data:
-            if user["id"] == other["id"]:
+
+        for other_key in data:
+            other = data[other_key]
+            if user_key == other_key:
                 continue
             farthest, index = farthest_close_neighbor(user)
             similarity = pearson.pearson(user, other)
 
             if len(user["neighbors"]) < n or similarity > farthest:
                 user["neighbors"].append({
-                    "id": other["id"],
+                    "id": other_key,
                     "similarity": similarity
                 })
             if len(user["neighbors"]) > n:
                 user["neighbors"].pop(index)
 
+# Get the items the users have not seen, but their neighbors have 
+# def unseen_items():
+
+
+# # Make a 
+# def predict():
+
 
 
 def generate(data, options):
     neighbors(data, options["n_similar_users"])
-    for i in range(10):
-        print("NAABURIT")
-        print(data[i]["id"], data[i]["neighbors"])
+    print("NAABURIT")
+    test_user = data[data.keys()[0]]
+    print(test_user)
+
+    
 
     # print(data)
 
@@ -60,9 +71,10 @@ def generate(data, options):
 # ]
 
 data = csv_reader.load_data("tools/shortest.tsv")
+# print(data)
 recommendations = generate(data, {
     "n_similar_users": 5
 })
 
-print(recommendations)
+# print(recommendations)
 
